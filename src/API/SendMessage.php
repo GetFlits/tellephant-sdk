8a004bbb881902e39d024ec3fb4ead3ed1c7ffb0
VrajPatel-1511 <159014228+VrajPatel-1511@users.noreply.github.com>
2024-07-05 12:18:15 +0530

    Fixed Typing Error (#3)
    * added tellephant sdk code
    
    * pending chnage
    
    * fixed typing error

--- src/API/SendMessage.php
+++ src/API/SendMessage.php
@@ -7,7 +7,7 @@ use Flits\Tellephant\TellephantProvider;
 class SendMessage extends TellephantProvider {
 
     public $METHOD = "POST";
-    public $URl = "send-message";
+    public $URL = "send-message";
 
     function __construct($config) {
         parent::__construct($config);
